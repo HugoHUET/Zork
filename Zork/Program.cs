2651b17bea2f8b7b37c0223c6b9bb2558f0f94fc
Paul <paulereverend@gmail.com>
2021-02-12 09:20:06 +0100

    [PLE] Create statup menu

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,7 +6,16 @@ namespace Zork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("#########################################################");
+            Console.WriteLine("#                        ZORK                           #");
+            Console.WriteLine("#                                                       #");
+            Console.WriteLine("#########################################################");
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("1- Create New Game");
+            Console.WriteLine("2- Load Saved Game");
+            Console.WriteLine("3- About");
+            Console.WriteLine("4- Exit");
+
         }
     }
 }
