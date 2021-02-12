8c0e248f17f0b78a5d1dec2f20a2e9736ef2851d
Paul <paulereverend@gmail.com>
2021-02-12 16:51:04 +0100

    [PLE] Init objects in create method

--- DataAccessLayer/Models/Game.cs
+++ DataAccessLayer/Models/Game.cs
@@ -6,12 +6,13 @@ namespace DataAccessLayer.Models
     public class Game
     {
         public int Id { get; set; }
-
+        public string name { get; set; }
         public Player player { get; set; }
         public List<Monster> Monsters { get; set; }
         public List<Object> inventory { get; set; }
 
-        public Weapon Weapon { get; set; }
+        public List<Weapon> Weapons { get; set; }
+        public List<Object> Objects { get; set; }
 
         public Game()
         {
--- Zork/Gameplay.cs
+++ Zork/Gameplay.cs
@@ -1,59 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using DataAccessLayer.Models;
 using Zork.Models;
 
 namespace Zork
 {
     public class Gameplay
     {
-        private static int TAILLE_PLATEAU = 10;
-        private List<List<int>> plateau;
         private string gameName;
 
-        public Gameplay(string gameName)
+        public Gameplay()
         {
-            this.gameName = gameName;
         }
 
-        public static void create()
+        public void create()
         {
-            string gn;
+            string gameName;
             do
             {
                 Console.WriteLine("Veuillez saisir le nom de la partie :");
-                gn = Console.ReadLine();
-            } while (string.IsNullOrWhiteSpace(gn));
+                gameName = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(gameName));
 
-            Random rnd = new Random();
-            Gameplay g = new Gameplay(gn);
-            int id = 0;
-            for (int i = 0; i < TAILLE_PLATEAU; i++)
-            {
-                for (int j = 0; j < TAILLE_PLATEAU; j++)
-                {
-                    int type = rnd.Next(0, 10);
-                    if(type >= 5 && type <= 7)
-                    {
-                        //loot
-                    }
-                    else if(type < 7)
-                    {
-                        //monster
-                    }
-                    g.plateau[i][j] = id;
-                    Console.Write($" {id}({i},{j}) ");
-                    id++;
-                }
-                Console.WriteLine();
-            }
+            this.gameName = gameName;
 
-            int playerPosX = rnd.Next(0, TAILLE_PLATEAU);
-            int playerPosY = rnd.Next(0, TAILLE_PLATEAU);
-            g.run();
+            Game game = new Game();
+            Player player = new Player();
+            Weapon rustedToothPick = new Weapon();
+            Weapon nerfGun = new Weapon();
+
+            DataAccessLayer.Models.Object painDeMie = new DataAccessLayer.Models.Object();
+            DataAccessLayer.Models.Object huileDeFoieDeMorue = new DataAccessLayer.Models.Object();
+            DataAccessLayer.Models.Object bananaSkin = new DataAccessLayer.Models.Object();
+
+            Monster kwoakGaming = new Monster();
+            Monster cSharpDev = new Monster();
+
+            game.Monsters.Add(kwoakGaming);
+            game.Monsters.Add(cSharpDev);
+
+            game.player = player;
+
+            game.Weapons.Add(rustedToothPick);
+            game.Weapons.Add(nerfGun);
+
+            game.Objects.Add(painDeMie);
+            game.Objects.Add(huileDeFoieDeMorue);
+            game.Objects.Add(bananaSkin);
+
+            game.name = this.gameName;
+
+            this.run();
         }
 
-        public static void load()
+        public void load()
         {
 
         }
@@ -75,36 +76,5 @@ namespace Zork
 
             Menu.DisplayMenu(options);
         }
-
-		private void getAvailableDeplacement(int playerPosX, int playerPosY) {
-            bool[] availableDeplacement = { false, false, false, false };
-            try
-            {
-                //go top
-                if(this.plateau.ElementAt(playerPosX).ElementAt(playerPosY + 1) != null)
-                {
-                    availableDeplacement[0] = true;
-                }
-
-                //go right
-                if(this.plateau.ElementAt(playerPosX + 1).ElementAt(playerPosY) != null)
-                {
-                    availableDeplacement[1] = true;
-                }
-
-                //go bottom
-                if(this.plateau.ElementAt(playerPosX).ElementAt(playerPosY - 1) != null)
-                {
-                    availableDeplacement[2] = true;
-                }
-
-                //go left
-                if(this.plateau.ElementAt(playerPosX - 1).ElementAt(playerPosY) != null)
-                {
-                    availableDeplacement[3] = true;
-                }
-            }
-            catch { }
-		}
     }
 }
