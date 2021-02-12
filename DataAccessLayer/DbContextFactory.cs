534adf32ed7973d884f2b430563ac18ad27c33d0
Hugo <hugo.huet.14@gmail.com>
2021-02-12 09:12:32 +0100

    init DataAccessLayer

---
+++ DataAccessLayer/DbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DataAccessLayer
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<ZorkDbContext>
+    {
+        public ZorkDbContext CreateDbContext(string[] args)
+        {
+            var dbContextBuilder = new DbContextOptionsBuilder<ZorkDbContext>();
+
+            dbContextBuilder.UseSqlServer("Server=localhost;Database=ZorkDb;Trusted_Connection=False;User ID=sa;Password=Root123@;",
+                opt => opt.MigrationsAssembly("DataAccessLayer"));
+
+            return new ZorkDbContext(dbContextBuilder.Options);
+        }
+    }
+}
---
+++ DataAccessLayer/ZorkDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class ZorkDbContext: DbContext
+    {
+        public ZorkDbContext(DbContextOptions<ZorkDbContext> options): base(options)
+        {
+        }
+    }
+}
