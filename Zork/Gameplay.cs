d0d8e813a80ac0a2b13110452ebffee4f4a7d57c
Paul <paulereverend@gmail.com>
2021-02-13 21:40:22 +0100

    [PLE] add all gameplay methods

--- Zork/Gameplay.cs
+++ Zork/Gameplay.cs
@@ -9,7 +9,7 @@ namespace Zork
 {
     public class Gameplay
     {
-        private string gameName;
+        private Game game;
         private readonly ZorkDbContext context;
 
         public Gameplay()
@@ -27,40 +27,44 @@ namespace Zork
                 gameName = Console.ReadLine();
             } while (string.IsNullOrWhiteSpace(gameName));
 
-            this.gameName = gameName;
 
-            Game game = new Game();
-            //Player player = new Player();
-            //Weapon rustedToothPick = new Weapon();
-            //Weapon nerfGun = new Weapon();
+            game = new Game(gameName);
 
-            //DataAccessLayer.Models.Object painDeMie = new DataAccessLayer.Models.Object();
-            //DataAccessLayer.Models.Object huileDeFoieDeMorue = new DataAccessLayer.Models.Object();
-            //DataAccessLayer.Models.Object bananaSkin = new DataAccessLayer.Models.Object();
+            // TODO : Sélecteur pour le pseudo et fichier de conf pour les hp de base
+            Player player = new Player("torti", 20, 0);
+            Weapon rustedToothPick = new Weapon("rustedToothPick", 5, 0.1);
+            Weapon nerfGun = new Weapon("nerfGun", 1, 0.5);
 
-            //Monster kwoakGaming = new Monster();
-            //Monster cSharpDev = new Monster();
+            DataAccessLayer.Models.Object painDeMie = new DataAccessLayer.Models.Object("painDeMie", 2, 1, 1);
 
-            //game.Monsters.Add(kwoakGaming);
-            //game.Monsters.Add(cSharpDev);
+            // Easter egg : L'huile de foie de morue est deg donc elle enlève des HP mais elle renforce a mort
+            DataAccessLayer.Models.Object huileDeFoieDeMorue = new DataAccessLayer.Models.Object("huileDeFoieDeMorue", -3, 3, 5);
+            DataAccessLayer.Models.Object bananaSkin = new DataAccessLayer.Models.Object("bananaSkin", 2, 1, 1);
 
-            //game.player = player;
+            Monster kwoakGaming = new Monster("kwoakGaming",3 , 0.2, 20);
+            Monster cSharpDev = new Monster("cSharpDev", 5, 0.1, 40);
 
-            //game.Weapons.Add(rustedToothPick);
-            //game.Weapons.Add(nerfGun);
+            game.Monsters.Add(kwoakGaming);
+            game.Monsters.Add(cSharpDev);
 
-            //game.Objects.Add(painDeMie);
-            //game.Objects.Add(huileDeFoieDeMorue);
-            //game.Objects.Add(bananaSkin);
+            game.player = player;
 
-            game.name = this.gameName;
+            game.Weapons.Add(rustedToothPick);
+            game.Weapons.Add(nerfGun);
+
+            game.UsedObjects.Add(painDeMie);
+            game.UsedObjects.Add(huileDeFoieDeMorue);
+            game.UsedObjects.Add(bananaSkin);
+
+            game = context.Games.Add(game).Entity;
+            this.context.SaveChanges();
 
             this.run();
         }
 
         public void load()
         {
-
+            game = context.Games.First();
         }
 
         private void run()
@@ -81,5 +85,84 @@ namespace Zork
 
             //Menu.DisplayMenu(options);
         }
+
+        private void useItem(DataAccessLayer.Models.Object item)
+        {
+            if (game.Inventory.Contains(item))
+            {
+                game.player.Hp += item.HPRestoreValue;
+                game.Inventory.Remove(item);
+                game.UsedObjects.Add(item);
+
+                this.updateGame();
+            }
+            else
+            {
+                Console.WriteLine("Vous ne possédez pas cet item");
+            }
+            
+        }
+
+        private void attack(Monster monster, Weapon weapon)
+        {
+            Random random = new Random();
+            if(random.NextDouble() > weapon.MissRate)
+            {
+                monster.Hp -= Convert.ToInt32(weapon.Damages * (1.0 + game.getTotalAttackBoost()));
+                if (monster.Hp <= 0)
+                {
+                    game.Monsters.Remove(monster);
+                    this.updateGame();
+                    Console.WriteLine(monster.Name + " est mort");
+                }
+                else
+                {
+                    context.Monsters.Update(monster);
+                    context.SaveChanges();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Oops ! Echec critique");
+            }
+        }
+
+        private void getAttacked(Monster monster)
+        {
+            Random random = new Random();
+            if (random.NextDouble() > monster.MissRate)
+            {
+                game.player.Hp -= Convert.ToInt32(monster.Damages * (1.0 - game.getTotalDefenseBoost()));
+                updateGame();
+            }
+            else
+            {
+                Console.WriteLine("Coup de bol ! " + monster.Name + " a fait un échec critique");
+            }
+        }
+
+        private Boolean shouldEndGame()
+        {
+            if (game.player.Hp <= 0)
+            {
+                Console.WriteLine("Vous avez perdu !");
+                return true;
+            }
+
+            if(game.Monsters.Count() == 0)
+            {
+                Console.WriteLine("Vous avez battu tous les monstres félicitations !");
+                return true;
+            }
+            return false;
+        }
+
+        // Met à jour la game dans la bdd
+        private void updateGame()
+        {
+            context.Games.Update(game);
+            context.SaveChanges();
+        }
+
     }
 }
